f752d8ca88f3f80c70870ff39c087c7fb46f2e86
Hate <ketchyp181995@mail.ru>
2017-05-03 22:10:19 +0400

    Server update

--- src/sample/Server.java
+++ src/sample/Server.java
@@ -5,9 +5,16 @@
  */
 package sample;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import static java.lang.System.in;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -17,6 +24,7 @@ import java.util.logging.Logger;
  */
 public class Server{
     private int PORT = 28960;                               //Порт сервера "По умолчанию"
+    private ArrayList<Client> clients = new ArrayList<Client>();
     
     public Server(){
         serverUp();                                         //Конструктор для запуска сервера с портом "По умолчанию"
@@ -33,12 +41,64 @@ public class Server{
             server = new ServerSocket(PORT);                //Попытка запустить сервер на порте PORT
             System.out.println("Server online");
         } catch (IOException ex) {
-            System.out.println("Error #1: Server offline");
+            System.out.println("Error #1: Server offline.");
             return;                                         //При неудачном запуске закрыть программу
         }
         
         while(true){
-            
+            try {
+                Socket serverUser = server.accept();
+                clients.add(new Client(serverUser));
+            } catch (IOException ex) {
+                Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
     }
+    
+    private class Client extends Thread{
+        private BufferedReader in = null;
+        private PrintWriter out = null;
+        private Socket clientSocket = null;
+        private String nickname = null;
+        
+        public Client(Socket clientSocket) throws IOException{
+            this.clientSocket = clientSocket;
+            try {
+                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                out = new PrintWriter(clientSocket.getOutputStream());
+            } catch (IOException ex) {
+                System.out.println("Error #2: input/output stream not responding.");
+                close();
+            }
+        }
+        
+        @Override
+        public void run(){
+            System.out.println("Client " + clientSocket.toString() + " cames now");
+            String message = "";
+            while(true){
+                try {
+                    message = in.readLine();
+                    for(Client client : clients){
+                        client.out.println(message);
+                    }
+                } catch (IOException ex) {
+                    System.out.println("Error #3: server connection lost.");
+                } finally {
+                    try {
+                        close();
+                    } catch (IOException ex) {
+                        System.out.println("Error #4: can't close stream.");
+                    }
+                    
+                }
+            }
+        }
+        
+        private void close() throws IOException{
+            in.close();
+            out.close();
+        }
+    }
+    
 }
