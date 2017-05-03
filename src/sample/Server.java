573696f556fe16339e8c9d9e368ce0cffe139721
Hate <ketchyp181995@mail.ru>
2017-05-03 22:44:43 +0400

    Server code comments

--- src/sample/Server.java
+++ src/sample/Server.java
@@ -7,16 +7,11 @@ package sample;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.io.PrintWriter;
-import static java.lang.System.in;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
@@ -24,7 +19,8 @@ import java.util.logging.Logger;
  */
 public class Server{
     private int PORT = 28960;                               //Порт сервера "По умолчанию"
-    private ArrayList<Client> clients = new ArrayList<Client>();
+    private ArrayList<Client> clients = 
+            new ArrayList<Client>();                        //Массив клиентов
     
     public Server(){
         serverUp();                                         //Конструктор для запуска сервера с портом "По умолчанию"
@@ -41,61 +37,71 @@ public class Server{
             server = new ServerSocket(PORT);                //Попытка запустить сервер на порте PORT
             System.out.println("Server online");
         } catch (IOException ex) {
-            System.out.println("Error #1: Server offline.");
+            System.out.println
+        ("Error #1: Server offline.");
             return;                                         //При неудачном запуске закрыть программу
         }
         
         while(true){
             try {
-                Socket serverUser = server.accept();
-                clients.add(new Client(serverUser));
+                Socket serverUser = server.accept();        //Сервер ждёт клиента
+                clients.add(new Client(serverUser));        //Подключившийся клиент добавляется в массив клиентов
             } catch (IOException ex) {
-                Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+                System.out.println
+        ("Error #2: Can't connect client.");
             }
         }
     }
     
-    private class Client extends Thread{
-        private BufferedReader in = null;
-        private PrintWriter out = null;
-        private Socket clientSocket = null;
-        private String nickname = null;
+    private class Client extends Thread{                    //Класс клиентов
+        private BufferedReader in = null;                   //Поток для чтения данных от клиента
+        private PrintWriter out = null;                     //Поток для отправки данных клиенту
+        private Socket clientSocket = null;                 //Сокет клиента
+        private String nickname = null;                     //Ник клиента(Будет изменено когда подключим БД)
         
         public Client(Socket clientSocket) throws IOException{
             this.clientSocket = clientSocket;
             try {
-                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                out = new PrintWriter(clientSocket.getOutputStream());
+                in = new BufferedReader(
+                        new InputStreamReader(
+                                clientSocket.getInputStream()));
+                out = new PrintWriter(
+                        clientSocket.getOutputStream());
             } catch (IOException ex) {
-                System.out.println("Error #2: input/output stream not responding.");
+                System.out.println
+        ("Error #3: input/output stream not responding.");
                 close();
             }
         }
         
         @Override
         public void run(){
-            System.out.println("Client " + clientSocket.toString() + " cames now");
-            String message = "";
+            System.out.println("Client "
+                    + clientSocket.toString() 
+                    + " cames now");                        //Оповещение о том, что клиент в зашел в чат(серверное)
+            String message = "";                            //Строка для сообщений клиента
             while(true){
                 try {
-                    message = in.readLine();
+                    message = in.readLine();                //Получаем сообщение клиента
                     for(Client client : clients){
-                        client.out.println(message);
+                        client.out.println(message);        //Отправляем полученное сообщение всем клиентам на сервере
                     }
                 } catch (IOException ex) {
-                    System.out.println("Error #3: server connection lost.");
+                    System.out.println
+        ("Error #4: server connection lost.");
                 } finally {
                     try {
                         close();
                     } catch (IOException ex) {
-                        System.out.println("Error #4: can't close stream.");
+                        System.out.println
+        ("Error #5: can't close stream.");
                     }
                     
                 }
             }
         }
         
-        private void close() throws IOException{
+        private void close() throws IOException{            //Метод для закрытия потоков чтения и записи клиента
             in.close();
             out.close();
         }
