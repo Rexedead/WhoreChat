a3aaf69068bc45c40be7806497ad71fb1881f316
Rexedead <2275788@gmail.com>
2017-05-06 06:57:12 +0300

    dbworker fix

--- src/sample/Server.java
+++ src/sample/Server.java
@@ -5,12 +5,9 @@
  */
 package sample;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
