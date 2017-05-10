7f7976c1f65e288fa9b0ca055a5c1b2a13c1a8e0
Hate <ketchyp181995@mail.ru>
2017-05-10 18:21:56 +0400

    add chatListModels for every messages

---
+++ src/sample/Client/ListsModels/FileMessageModel.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sample.Client.ListsModels;
+
+import java.io.File;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+/**
+ *
+ * @author Hate
+ */
+public class FileMessageModel extends MessageListModel{
+    private ImageView MSGData = new ImageView();
+    private File DATA;
+    
+    public FileMessageModel(){
+        MSGData.setFitHeight(20);
+        MSGData.setFitWidth(20);
+        getChildren().add(MSGData);
+    }
+    
+    public void setDATA(File DATA){
+        this.DATA = DATA;
+        MSGData.setImage(new Image("file:/sample/resources/FileMessageIcon.png"));
+    }
+    
+    public File getDATA(){return DATA;}
+}
